f3dbcb33c9bfb822e66f9508e46f18c70d49c34b
CelineTrammi <celinetram@gmail.com>
2025-11-17 13:55:26 +0100

    feat: add harpoon plugin

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -371,4 +371,29 @@ return {
 			end, { desc = "[S]earch [N]eovim files" })
 		end,
 	},
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+		local harpoon = require("harpoon")
+
+		-- REQUIRED: initialize Harpoon
+		harpoon:setup()
+
+		-- Example keymaps
+		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end, { desc = "Harpoon file" })
+		vim.keymap.set("n", "<leader>l", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = "Harpoon quick menu" })
+
+		-- Navigate between files
+		vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+		vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+		vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+		vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
+		vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
+		end,
+	}
 }
